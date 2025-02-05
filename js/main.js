2b18082183a777b7a2e7b18f503d171272c2517b
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-02-05 13:32:22 +0300

    сделал основной компонент родитель вью,начал делать компонент первой колонки,пока что сделал передачу от родительского компонента карточек и готовлю шаблон

--- js/main.js
+++ js/main.js
@@ -1,6 +1,57 @@
-let app = new Vue ({
-    el: '#app',
+Vue.component('firstColumn', {
+    props: ['cards'],
+    template: `
+    <div class="firstColumn">
+        <h2>Колонка 1</h2>
+        <div v-for="(card, index) in cards" :key="card.id" class="card">
+            <h3>{{ card.title }}</h3>
+            <ul>
+                <li v-for="(item, i) in card.items" :key="i">
+                    <input type="checkbox" @change="checkComplection(card)"> {{ item }}
+                </li>
+            </ul>
+        </div>
+        <form @submit.prevent="addCard">
+            <input v-model="newCardTitle" placeholder="Заголовок карточки" required>
+        </form>
+    </div>`,
     data: {
+        return {
+            cards: [
 
+            ]
+        }
     }
 })
+
+new Vue ({
+    el: '#app',
+    data() {
+        return {
+            firstColumnCards:JSON.parse(localStorage.getItem('firstColumn') || '[]'),
+        };
+    },
+    methods: {
+        addCardToFirstColumn(card) {
+            if(this.firstColumnCards.length < 3){
+                this.firstColumnCards.push(card);
+                this.saveToLocalStorage();
+            }
+        },
+        clearData() {
+            localStorage.clear();
+            this.firstColumnCards = [];
+        },
+        saveToLocalStorage() {
+            localStorage.setItem('firstColumn', JSON.stringify(this.firstColumnCards));
+        }
+    },
+    watch: {
+        firstColumnCards: {
+            handler() {
+                this.saveToLocalStorage();
+            },
+            deep: true,
+        }
+    }
+});
